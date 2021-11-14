892928c00b7f11fe7194b0eae15bc43527b6c509
Xrom-dmitriy <dmitriykhromov71@mail.ru>
2021-11-14 11:37:33 +0300

    acp

---
+++ AlphaChannelProcessing/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace AlphaChannelProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.trackR = new System.Windows.Forms.TrackBar();
+            this.trackG = new System.Windows.Forms.TrackBar();
+            this.trackB = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.menuStrip1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.trackB);
+            this.splitContainer1.Panel2.Controls.Add(this.trackG);
+            this.splitContainer1.Panel2.Controls.Add(this.trackR);
+            this.splitContainer1.Panel2.Controls.Add(this.trackBar1);
+            this.splitContainer1.Size = new System.Drawing.Size(791, 450);
+            this.splitContainer1.SplitterDistance = 531;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(531, 420);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(531, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 26);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(20, 28);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(56, 396);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // trackR
+            // 
+            this.trackR.Location = new System.Drawing.Point(82, 27);
+            this.trackR.Name = "trackR";
+            this.trackR.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackR.Size = new System.Drawing.Size(56, 396);
+            this.trackR.TabIndex = 1;
+            this.trackR.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            this.trackR.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // trackG
+            // 
+            this.trackG.Location = new System.Drawing.Point(144, 27);
+            this.trackG.Name = "trackG";
+            this.trackG.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackG.Size = new System.Drawing.Size(56, 396);
+            this.trackG.TabIndex = 2;
+            this.trackG.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // trackB
+            // 
+            this.trackB.Location = new System.Drawing.Point(196, 28);
+            this.trackB.Name = "trackB";
+            this.trackB.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackB.Size = new System.Drawing.Size(56, 396);
+            this.trackB.TabIndex = 3;
+            this.trackB.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "0%";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackG;
+        private System.Windows.Forms.TrackBar trackR;
+        private System.Windows.Forms.TrackBar trackB;
+    }
+}
+
---
+++ AlphaChannelProcessing/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlphaChannelProcessing
+{
+    public partial class Form1 : Form
+    {
+        string filename;
+        Bitmap bmp;
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = 255;
+            openFileDialog1.Filter = "Image file(*.png)|*.png|All files(*.*)|*.*";
+            trackBar1.Value = 1;
+            trackR.Minimum = 0;
+            trackG.Minimum = 0;
+            trackB.Minimum = 0;
+            trackR.Maximum = 100;
+            trackG.Maximum = 100;
+            trackB.Maximum = 100;
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Text = $"{(double)trackBar1.Value/255*100}%";
+            
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            filename = openFileDialog1.FileName;
+            pictureBox1.Image = Image.FromFile(filename);
+            bmp = new Bitmap(pictureBox1.Image);
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            if (bmp == null)
+                return;
+            pictureBox1.Image = imageColor(new Bitmap(filename), trackBar1.Value, trackR.Value / 100.0, trackG.Value / 100.0, trackB.Value / 100.0);
+            
+        }
+        private Image imageColor(Bitmap image, int opacity, double R, double G, double B)
+        {
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    Color c = image.GetPixel(w, h);
+                    if (c != Color.Transparent) 
+                    {
+                        Color newC = Color.FromArgb(opacity , (int)(c.R * R), (int)(c.G * G), (int)(c.B * B));
+                        image.SetPixel(w, h, newC);
+                    }
+                }
+            }
+            return image;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
